67ba857887e40b3ae34e28e0f2bb536f17ef0eec
Kresten Laust <krestenlaust@gmail.com>
2023-12-26 19:07:05 +0100

    Added sources and cleaned entrypoint

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,12 @@
-import com.typesafe.scalalogging
 import com.typesafe.scalalogging.Logger
+//import com.novamaday.d4j.gradle.simplebot.listeners.SlashCommandListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 
 val logger = Logger("Discord-Server-Bridge")
 
 @main def hello: Unit =
-  logger.debug("Hello world")
-  println("Hello world!")
-  println(msg)
+  logger.debug(msg)
 
 def msg = "I was compiled by Scala 3. :)"
