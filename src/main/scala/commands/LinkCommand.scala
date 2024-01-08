8c037c12de7e8278c15bcebe30eae83e00bdd7fa
Kresten Laust <krestenlaust@gmail.com>
2024-01-09 00:42:06 +0100

    Perfectly handled optionals, and link command works now

--- src/main/scala/commands/LinkCommand.scala
+++ src/main/scala/commands/LinkCommand.scala
@@ -1,5 +1,8 @@
 package discordserverbridge.commands
 
+import discord4j.common.util.Snowflake
+import discord4j.core.`object`.entity.channel.MessageChannel
+import discordserverbridge.LinkedMessageChannelManager
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import reactor.core.publisher.Mono
 
@@ -7,7 +10,28 @@ class LinkCommand extends SlashCommand:
   override def name: String = "link"
 
   override def handle(event: ChatInputInteractionEvent): Mono[Void] =
-    // Reply to the slash command, with the name the user supplied
-    event.reply()
-      .withEphemeral(true)
-      .withContent("Hello, hello there")
+    var destinationChannelSnowflake: Option[Snowflake] = None
+
+    try {
+      destinationChannelSnowflake = Option(Snowflake.of(event.getOption("destination").get().getValue.get().asString()))
+    }catch
+      case _ => return event.reply().withEphemeral(true).withContent("Invalid destination channel :(")
+
+    destinationChannelSnowflake match
+      case Some(snowflake: Snowflake) =>
+        event.getClient.getGuilds
+          .flatMap(guild => guild.getChannelById(snowflake).cast(classOf[MessageChannel])
+          .flatMap(destinationChannel => {
+            event.getInteraction.getChannel.flatMap(originChannel => {
+              // Now you have a reference to the destination channel
+              // Do whatever you need with the destination channel here
+
+              // Example: Reply with a message in the destination channel
+              //destinationChannel.createMessage("Hello, hello there")
+              LinkedMessageChannelManager.linkChannel(originChannel, destinationChannel)
+              event.reply().withEphemeral(true).withContent("Successfully linked channels")
+            });
+          })
+        ).`then`(Mono.empty)
+      case None =>
+        event.reply().withEphemeral(true).withContent("Invalid destination channel")
