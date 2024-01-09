c64e3902ad0f253e967e4747ee1333ed0ec18a76
Kresten Laust <krestenlaust@gmail.com>
2024-01-10 00:09:33 +0100

    Partially supports message passing now

--- src/main/scala/discordserverbridge/ChatMessageListener.scala
+++ src/main/scala/discordserverbridge/ChatMessageListener.scala
@@ -1,12 +1,30 @@
 package discordserverbridge
 
 import com.typesafe.scalalogging.Logger
+import discord4j.core.`object`.entity.{Message, User}
+import discord4j.core.`object`.entity.channel.MessageChannel
 import discord4j.core.event.domain.message.MessageCreateEvent
 import reactor.core.publisher.Mono
+
+import scala.jdk.OptionConverters.*
 
 object ChatMessageListener:
   val logger: Logger = Logger(getClass.getName)
 
-  def handle(event: MessageCreateEvent): org.reactivestreams.Publisher[Object] =
+  def handle(event: MessageCreateEvent): Mono[Void] =
     logger.info(event.getMessage.toString)
-    Mono.empty
+
+    event.getMessage.getAuthor.asScala.flatMap { author =>
+      if author.isBot then
+        return Mono.empty
+      else
+        None
+      }
+
+    event.getMessage.getChannel.flatMap { (originChannel: MessageChannel) =>
+      LinkedMessageChannelManager.linkedChannelByOrigin(originChannel) match
+        case Some(destChannel) =>
+          destChannel.createMessage(s"${event.getMessage.getContent}")
+        case None =>
+          Mono.empty[Message]
+    }.`then`(Mono.empty)
